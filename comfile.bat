775ffc68ad8ddb146b111ea1465ede342c6a8895
lilly <lilly1987@naver.com>
2021-03-06 22:22:36 +0900

    초기 커밋.

---
+++ comfile.bat
@@ -0,0 +1,6 @@
+C:\Windows\Microsoft.NET\Framework\v3.5\csc ^
+/t:library ^
+/lib:"F:\COM3D2\COM3D2x64_Data\Managed","f:\COM3D2\Sybaris\UnityInjector","f:\COM3D2\Sybaris","G:\com3d2\_plugin\cm3d2_j_154\Sybaris\UnityInjector","G:\com3d2\_plugin\cm3d2_j_154\Sybaris" ^
+/r:UnityEngine.dll /r:UnityInjector.dll /r:Assembly-CSharp.dll /r:Assembly-CSharp-firstpass.dll ^
+/r:ExIni.dll /r:COM3D2.ExternalSaveData.Managed.dll /r:PluginExt.dll COM3D2.VibeYourMaid.Plugin.cs
+pause
